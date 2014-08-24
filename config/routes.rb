78f120282ec0387f22a3dc70d8e288c8279762c0
Andrew Butler <abutler3@gmail.com>
2014-08-24 18:30:57 -0500

    Changes before final run through

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,7 @@ Rails.application.routes.draw do
   end
 
   devise_for :users
-  scope "/admin" do
-    resources :users
-  end
+    
   root 'welcome#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
